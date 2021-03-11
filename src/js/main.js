5f8ecf51d02670bf4ded00a4d56a26e0991b9985
Lotpite <exstornado@gmail.com>
2021-03-11 11:40:16 +0300

    Added from loc stor

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-console.log('Hi');
+import modals from './modules/modals';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
+import loadMoreItems from './modules/loadMoreItems';
+import calc from './modules/calc';
+import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
+import burger from './modules/burger';
+import scrolling from './modules/scrolling';
+import drop from './modules/drop';
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    modals();
+    slider('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    slider('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    loadMoreItems('.button-styles', '#styles .row');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
+    filter();
+    pictureSize('.sizes-block');
+    accordion('.accordion-heading');
+    burger('.burger', '.burger-menu');
+    scrolling('.pageup');
+    drop();
+});
